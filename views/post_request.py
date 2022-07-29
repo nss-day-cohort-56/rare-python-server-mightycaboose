c38bb752f510cd7d9a634379704f495b38f2df4a
Michael O'Brien <michael.obrien45@gmail.com>
2022-07-29 15:50:31 -0500

    current-post-edit

--- views/post_request.py
+++ views/post_request.py
@@ -64,7 +64,6 @@ def get_posts_by_user_id(user_id):
         WHERE user_id = ?
         """, ( user_id, ))
 
-
         posts = []
         dataset = db_cursor.fetchall()
 
@@ -74,3 +73,35 @@ def get_posts_by_user_id(user_id):
             posts.append(post.__dict__)
 
     return json.dumps(posts)
+
+def get_single_post(id):
+    """get single post"""
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+       SELECT
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.publication_date,
+            p.image_url,
+            p.content,
+            p.approved
+        FROM Posts p
+        WHERE p.id = ?
+        """, (id, ))
+
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        post = Post(data['id'], data['user_id'], data['category_id'],
+                        data['title'], data['publication_date'],
+                        data['image_url'], data['content'], data['approved'])
+
+        return json.dumps(post.__dict__)
