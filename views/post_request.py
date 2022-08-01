497c4479515ff44d7648c55a25f29455594a9848
Caleb Gore <cgore2001@gmail.com>
2022-08-01 13:46:36 -0500

    modified post_request to add get_single_post

--- views/post_request.py
+++ views/post_request.py
@@ -1,11 +1,12 @@
 import sqlite3
 import json
-from models import Post
+from models import Category, User, Post
+
 
 def get_all_posts():
     # Open a connection to the database
     with sqlite3.connect("./db.sqlite3") as conn:
-    
+
         # Just use these. It's a Black Box.
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
@@ -37,12 +38,14 @@ def get_all_posts():
             # Note that the database fields are specified in
             # exact order of the parameters defined in the
             # Animal class above.
-            post = Post(row['id'], row['user_id'], row['category_id'], row['title'], row['publication_date'], row['image_url'], row['content'], row['approved'])
+            post = Post(row['id'], row['user_id'], row['category_id'], row['title'],
+                        row['publication_date'], row['image_url'], row['content'], row['approved'])
 
             posts.append(post.__dict__)
 
     # Use `json` package to properly serialize list as JSON
     return json.dumps(posts)
+
 
 def get_posts_by_user_id(user_id):
     """ please"""
@@ -62,21 +65,21 @@ def get_posts_by_user_id(user_id):
             p.approved
         FROM Posts p
         WHERE user_id = ?
-        """, ( user_id, ))
-
+        """, (user_id, ))
 
         posts = []
         dataset = db_cursor.fetchall()
 
         for row in dataset:
             post = Post(row['id'], row['user_id'], row['category_id'], row['title'],
-                            row['publication_date'], row['image_url'], row['content'], row['approved'])
+                        row['publication_date'], row['image_url'], row['content'], row['approved'])
             posts.append(post.__dict__)
 
     return json.dumps(posts)
 
+
 def get_single_post(id):
-    """get single entry"""
+    """get single post"""
     with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
@@ -92,22 +95,48 @@ def get_single_post(id):
             p.publication_date,
             p.image_url,
             p.content,
-            p.approved
+            p.approved,
+            u.first_name user_first_name,
+            u.last_name user_last_name,
+            u.email user_email,
+            u.bio user_bio,
+            u.username user_username,
+            u.password user_password,
+            u.profile_image_url  user_profile_image_url,
+            u.created_on  user_created_on,
+            u.active user_active
         FROM Posts p
+        JOIN Users u
+            on u.id = p.user_id
         WHERE p.id = ?
         """, (id, ))
 
         # Load the single result into memory
-        row = db_cursor.fetchone()
+        data = db_cursor.fetchone()
 
         # Create an animal instance from the current row
-        post = Post(row['id'],
-                    row['user_id'],
-                    row['category_id'],
-                    row['title'],
-                    row['publication_date'],
-                    row['image_url'],
-                    row['content'],
-                    row['approved'])
+        post = Post(data['id'],
+                    data['user_id'],
+                    data['category_id'],
+                    data['title'],
+                    data['publication_date'],
+                    data['image_url'],
+                    data['content'],
+                    data['approved'])
+
+        user = User(
+            data['user_id'],
+            data['user_first_name'],
+            data['user_last_name'],
+            data['user_email'],
+            data['user_bio'],
+            data['user_username'],
+            data['user_password'],
+            data['user_profile_image_url'],
+            data['user_created_on'],
+            data['user_active']
+        )
+
+        post.user = user.__dict__
 
         return json.dumps(post.__dict__)
